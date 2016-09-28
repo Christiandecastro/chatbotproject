5ac9ba7db1716cc1d3109d109d7450f6282a5a3b
christian de castro <robert12102000@yahoo.com>
2016-09-28 10:49:02 -0700

    added multiple responses
    added more questions to the bot of raps.

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,13 @@
 function askQuestion() {
   var input = document.getElementById("input").value;
-   document.getElementById("chat-area").textContent += input;
+  var giveresponse = responses[input]
+ document.getElementById("chat-area").textContent += input + ": " + giveresponse + " ";
+}
+
+
+var responses = {
+ "what's your name?" : "My names RapBot. Just spitting hot rhymes while taking precious time. Just sitting on internet doing zero cyber crimes.",
+ "how are you?" : "I'm good why thanks for your concern. Thanks for caring for my gratitude you have earned.",
+ "Can I beat you in a rap battle?" : "HA! Thanks for making laugh that's such a good joke. My raps are faster than you can catch up you slow poke.You can never beat me in a million years. What my roasts consits of you crying with a million tears."
+
 }
